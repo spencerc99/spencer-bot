acea0537f9f2723d4931f201840354e021e1c5bd
Spencer Chang <Spencer.Chang@rice.edu>
2015-10-07 18:22:46 -0500

    Update bot.js
    Responding to Ugh

--- bot.js
+++ bot.js
@@ -8,9 +8,14 @@ function respond() {
       botRegex = /^spencer bot$/;
       botRegex2 = /^Ugh$/;
 
-  if(request.text && (botRegex.test(request.text) || botRegex2.test(request.text))) {
+  if(request.text && botRegex.test(request.text)) {
     this.res.writeHead(200);
-    postMessage(request);
+    postMessage();
+    this.res.end();
+  }
+  else if(request.text && botRegex2.test(request.text)) {
+    this.res.writeHead(200);
+    postMessage2();
     this.res.end();
   } else {
     console.log("don't care");
@@ -19,7 +24,7 @@ function respond() {
   }
 }
 
-function postMessage(request) {
+function postMessage() {
   var botResponse, options, body, botReq;
   if(request.text=="Ugh") {
     botReponse = "I'm sorry about that :("
@@ -56,5 +61,38 @@ function postMessage(request) {
   botReq.end(JSON.stringify(body));
 }
 
+function postMessage2() {
+  var botResponse, options, body, botReq;
+  botReponse = "I'm sorry about that :("
+  
+  options = {
+    hostname: 'api.groupme.com',
+    path: '/v3/bots/post',
+    method: 'POST'
+  };
+
+  body = {
+    "bot_id" : botID,
+    "text" : botResponse
+  };
+
+  console.log('sending ' + botResponse + ' to ' + botID);
+
+  botReq = HTTPS.request(options, function(res) {
+      if(res.statusCode == 202) {
+        //neat
+      } else {
+        console.log('rejecting bad status code ' + res.statusCode);
+      }
+  });
+
+  botReq.on('error', function(err) {
+    console.log('error posting message '  + JSON.stringify(err));
+  });
+  botReq.on('timeout', function(err) {
+    console.log('timeout posting message '  + JSON.stringify(err));
+  });
+  botReq.end(JSON.stringify(body));
+}
 
 exports.respond = respond;
