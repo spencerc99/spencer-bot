982b69a1b3f23b3faa968d212f97396bdca1a9f7
Spencer Chang <spencerc99@gmail.com>
2015-10-28 16:33:43 -0500

    Revert "Responding to other commands 2"
    This reverts commit b7d963aaa3d107c6d60f3cbf15d57262831f3007.

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ function respond() {
 
   if(request.text && (botRegex.test(request.text) || botRegex2.test(request.text))) {
     this.res.writeHead(200);
-    postMessage(request.text);
+    postMessage();
     this.res.end();
   } else {
     console.log("don't care");
